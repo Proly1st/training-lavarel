380d5f8fa7563c09ee9782c52211926a021af367
Proly1st <133683363+Proly1st@users.noreply.github.com>
2023-12-15 14:52:34 +0700

    sua bai 1 va bai 2

--- assets/bai2Alert.js
+++ assets/bai2Alert.js
@@ -1,5 +1,12 @@
   // bai 2 hien alert khi click vao button
   function btnclickBai2(){
+    
     var inputValue = document.getElementById("bai2-input").value;
-    alert(inputValue);
+    if(inputValue === null || inputValue === undefined || inputValue === '' ){
+        alert("Vui lòng nhập vào giá trị")
+    }
+    else if(!isNaN(inputValue)){
+        alert(inputValue);
+
+    }
 }
--- assets/bai3Sum2so.js
+++ assets/bai3Sum2so.js
@@ -3,6 +3,13 @@
 function btnSum(){
     var input1 = Number( document.getElementById("input1").value);
     var input2 = Number( document.getElementById("input2").value);
-    var sum = input1+input2;
-    alert( "Tổng 2 số là: "+ sum);
+    if(input1 === null || input1 === undefined || input1 === '' ||input2 === null || input2 === undefined || input2 === ''){
+        alert("Vui lòng nhập vào giá trị")
+    }
+    else if(!isNaN(input1) && !isNaN(input1)  ){
+         var sum = input1+input2;
+         alert( "Tổng 2 số là: "+ sum);
+    }
+    
+   
 }
