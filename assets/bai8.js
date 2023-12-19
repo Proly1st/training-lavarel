e412c055c8dfcbf01c85268711f6268e7f42bb44
Proly1st <133683363+Proly1st@users.noreply.github.com>
2023-12-19 20:18:39 +0700

    sua bai 8

--- assets/bai8.js
+++ assets/bai8.js
@@ -15,73 +15,6 @@
 // // vd: data-a = "avc"
 // // cách lấy sẽ khác.
 
-// $(function(){
-//     // dòng dưới là lấy ra mấy giá trị mà jquery nó thêm vào nên ghi là document, ở giữa thêm id của bảng.
-//     $(document).on('change','#b8Select',function(){
-//         let stt = $('#b8Table tbody').children().length+1;
-//        //Cụ thể, $('#b8Table tbody') sẽ chọn phần tử <tbody> trong bảng có ID là b8Table. Với tiếp theo, .children() sẽ trả về tất cả các phần tử con (children) của phần tử <tbody>. Cuối cùng, .length sẽ trả về số lượng các phần tử con đó. 
-        
-//         let selectedOption = $(this).find('option:selected');
-//         // selectedOption lấy ra cái option đã chọn
-//         let name = $('#b8Select').val();
-//         let soluong = parseInt(selectedOption.data('soluong')) ;
-//         // lấy cái data có Attribute là trong dấu '' 
-//         let donvi = selectedOption.data('donvi') ;
-//         let donGia = parseInt (selectedOption.data('gia'));
-//         let thanhtien = donGia;
-
-       
-//         //tạo một cột (<td>) mới trong hàng với nội dung là giá trị của biến stt và gán cho biến cotStt.
-//         let cotStt =  $('<td>' + stt +'</td>');
-//         let cotTen = $('<td>' + name +'</td>');
-//         let cotSL = $('<td><input class="sLnew" type="number" value="' + soluong + '"></input></td>');
-//         let cotGia = $('<td>' + donGia +'</td>');
-//         let cotDonVi = $('<td>' + donvi +'</td>');
-//         let cotTTien = $('<td>' + thanhtien +'</td>');
-//         let cotAction = $('<td> <button class="btn-xoa" >Xóa</button></td>');
-        
-//         let newRow = $('<tr></tr>');
-//         // tạo ra 1 dòng mới r thêm cột đã tạo vào trong.
-//         newRow.append(cotStt);
-//         newRow.append(cotTen);
-//         newRow.append(cotSL);
-//         newRow.append(cotGia);
-//         newRow.append(cotDonVi);
-//         newRow.append(cotTTien);
-//         newRow.append(cotAction);
-//         // thêm hàng mới vào phần thân bảng
-//         $('#b8Table tbody').append(newRow);
-//     });
-// });
-
-// $(function(){
-//     $(document).on('input paste','.sLnew',function(){
-//         soluong = parseInt( $(this).val());
-        
-//         if(soluong <1){
-       
-//             soluong=1;
-//             alert("Số lượng thấp nhất là: 1")
-            
-//         }
-//         donGia = $(this).parents('tr').find('td:eq(3)').text();  
-//         thanhtien = donGia  * soluong ;
-//         $(this).parents('tr').find('td:eq(5)').text(thanhtien);
-//        //parents('tr') tìm hàng tr chứa phần tử hiện tại. find('td:eq(5)') thì tìm kiếm ô số 5 trong hàng <tr> .
-//     });
-// });
-
-// $(function(){
-//     $(document).on('click','.btn-xoa',function(){
-        
-//         $(this).closest('tr').remove();
-        
-//         $('#b8Table tbody tr').each(function(index) {
-//             $(this).find('td:eq(0)').text(index + 1);
-//           });
-// //.each() để lặp qua các hàng trong <tbody> của bảng. mỗi lần lặp là nó tìm phần tử td cột 1 rồi cập nhật lại. nãy làm không ra là do ở trên có 'tr ' rồi nhưng mà ở dưới vẫn sài parents nên nó k ra.
-//     });
-// });
 
 $(function(){
     function addRow() {
@@ -92,7 +25,7 @@ $(function(){
       let donvi = selectedOption.data('donvi');
       let donGia = parseInt(selectedOption.data('gia'));
       let thanhtien = donGia * soluong;
-  
+      
       let cotStt = $('<td>' + stt + '</td>');
       let cotTen = $('<td>' + name + '</td>');
       let cotSL = $('<td><input class="sLnew" type="number" value="' + soluong + '"></input></td>');
@@ -142,12 +75,13 @@ $(function(){
        
         let tongThanhTien = tongtien +thue ;
         if(giamGia>tongThanhTien){
+            $('#inputGiamGia').val(0);
             alert('giảm giá không được lớn hơn tổng tiền');
+            giamGia=0;
         }
         tongThanhTien -= giamGia;
         if(tongThanhTien<0){
-            tongThanhTien=0;
-            
+            tongThanhTien=0;  
         }
         $('#TongTTien').text("Tổng thành tiền: "+tongThanhTien);
 
@@ -167,9 +101,13 @@ $(function(){
         soluong = 1;
         alert("Số lượng thấp nhất là: 1");
       }
+     
   
       let donGia = $(this).parents('tr').find('td:eq(3)').text();
       let thanhtien = donGia * soluong;
+      if(isNaN(thanhtien)){
+        thanhtien=0;
+      }
       $(this).parents('tr').find('td:eq(5)').text(thanhtien);
       updateHoaDon();
     });
